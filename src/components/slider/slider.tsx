a69929a86af298a1ff4820734e508d80cc04ff25
Dokuyaku <sk8baz@gmail.com>
2023-12-22 18:14:35 +0100

    commit main 9.3

--- src/components/slider/slider.tsx
+++ src/components/slider/slider.tsx
@@ -1,6 +1,7 @@
 'use client';
 import SimpleImageSlider from "react-simple-image-slider";
-
+import './slider.css';
+import { useEffect, useState } from "react";
 // Définir un type pour une image
 interface ImageType {
     url: string;
@@ -11,14 +12,29 @@ interface SliderProps {
     images: ImageType[];
 }
 export default function  Slider  ({ images }: SliderProps)  {
+    const [sliderSize, setSliderSize] = useState({ width: 600, height: 600 });
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth < 600) {
+                setSliderSize({ width: window.innerWidth, height: window.innerWidth });
+            } else {
+                setSliderSize({ width: 600, height: 600 });
+            }
+        };
+
+        window.addEventListener('resize', handleResize);
+        handleResize();
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
     if (!Array.isArray(images) || images.length === 0) {
         return null; // ou retourner un élément par défaut si nécessaire
     }
   return (
-    <div>
+    <div className="sliderContainer">
       <SimpleImageSlider
-        width={600}
-        height={600}
+        width={sliderSize.width}
+        height={sliderSize.height}
         images={images}
         showBullets={false}
         showNavs={true}
